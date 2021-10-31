82333d26b966e41af08895389a56c0e0b68461a5
devteni <fatunmbiteniola001@gmail.com>
2021-10-31 16:05:29 +0100

    [feat]: fixed server start up

--- server.js
+++ server.js
@@ -1,13 +1,13 @@
 require('dotenv').config();
 const http = require('http');
 const app = require('./app');
-const { database } = require('./config/database');
+const database = require('./config/database');
 const { PORT } = process.env;
 
 const server = http.createServer(app);
 
 const runServer = async() => {
-    // database();
+    database();
     server.listen(PORT, () => {
         console.log(`Server is running on port ${PORT}`);
     });
