1bedc1a0f7271daaef0b60baf5f9fec895443bb4
devteni <fatunmbiteniola001@gmail.com>
2021-10-31 17:16:48 +0100

    [feat]: added login route to the authentication routes

--- routes/auth.routes.js
+++ routes/auth.routes.js
@@ -1,7 +1,8 @@
 const { Router } = require('express');
-const { signUp } = require('../controllers/auth.controller');
+const { signUp, logIn } = require('../controllers/auth.controller');
 const router = Router();
 
 router.post('/signup', signUp);
+router.post('/login', logIn);
 
 module.exports = router;
