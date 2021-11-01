d0d4996c4743fcc6e8091ac7e7aa54ddca562987
devteni <fatunmbiteniola001@gmail.com>
2021-11-01 21:32:11 +0100

    [feat]: added /user route

--- routes/index.routes.js
+++ routes/index.routes.js
@@ -1,7 +1,9 @@
 const authRoutes = require('./auth.routes');
+const userRoutes = require('./user.routes');
 const { Router } = require('express');
 const router = Router();
 
 router.use('/api/', authRoutes);
+router.use('/user/', userRoutes);
 
 module.exports = router;
