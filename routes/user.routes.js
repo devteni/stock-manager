cc33bdf72fe75a7ccd180178bf882e38c95d6fb6
devteni <fatunmbiteniola001@gmail.com>
2021-11-01 21:31:39 +0100

    [feat]: implemented portfolio related routes

---
+++ routes/user.routes.js
@@ -0,0 +1,12 @@
+const { Router } = require('express');
+const verifyToken = require('../middlewares/verifyToken');
+const { viewProfile, updateProfile, viewPortfolio, viewLoanProfile, addAssets } = require('../controllers/user.controller');
+const router = Router();
+
+router.get('/profile', verifyToken, viewProfile);
+router.post('/profile', verifyToken, updateProfile);
+router.post('/portfolio/add', addAssets);
+router.get('/portfolio', verifyToken, viewPortfolio)
+router.get('/loan', verifyToken, viewLoanProfile);
+
+module.exports = router;
