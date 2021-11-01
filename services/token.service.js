54a9ab64b21abb4bcbb5cc557956ebb65e2cdda9
devteni <fatunmbiteniola001@gmail.com>
2021-11-01 21:29:36 +0100

    [feat]: implemented decodeUserWithToken service to get user details from auth token

--- services/token.service.js
+++ services/token.service.js
@@ -1,7 +1,8 @@
 const jwt = require('jsonwebtoken');
 const AppError = require('../utils/AppError');
 const { JWT_SECRET, JWT_EXPIRY } = process.env;
-exports.createToken = (user) => {
+
+const createToken = (user) => {
     try {
         let token = jwt.sign({
             id: user._id,
@@ -16,11 +17,25 @@ exports.createToken = (user) => {
     }
 }
 
-exports.decodeToken = (token) => {
+const decodeToken = (token) => {
     try {
         let decodedToken = jwt.verify(token, JWT_SECRET);
+        // if(!decodedToken) return 'Wrong or expired token';\
         return decodedToken;
     } catch (err) {
         throw new AppError('Error while identifying user', 500);
     }
 }
+
+const decodeUserWithToken = (token) => {
+    try{
+        // decode the token
+        let userID = decodeToken(token);
+        return userID;
+    } catch(error){
+        throw new AppError('Error while getting user identity', 500);
+    }
+}
+
+
+module.exports = { createToken, decodeToken, decodeUserWithToken };
