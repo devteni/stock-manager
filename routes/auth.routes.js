ed4e673f1a802b34dc856e204af5dc0df74b9aef
devteni <fatunmbiteniola001@gmail.com>
2021-11-05 00:12:33 +0100

    [refactor]: updated api routes

--- routes/auth.routes.js
+++ routes/auth.routes.js
@@ -1,8 +1,12 @@
 const { Router } = require('express');
-const { signUp, logIn } = require('../controllers/auth.controller');
+const { signUp, logIn } = require('../controllers/Auth/auth.controller');
+const { viewProfile, updateProfile } = require('../controllers/Auth/profile.controller');
+const verifyToken = require('../middlewares/verifyToken');
 const router = Router();
 
 router.post('/signup', signUp);
 router.post('/login', logIn);
+router.get('/profile', verifyToken, viewProfile);
+router.post('/profile', verifyToken, updateProfile);
 
 module.exports = router;
--- routes/user.routes.js
+++ routes/user.routes.js
@@ -1,10 +1,9 @@
 const { Router } = require('express');
 const verifyToken = require('../middlewares/verifyToken');
-const { viewProfile, updateProfile, viewPortfolio, viewLoanProfile, addAssets } = require('../controllers/user.controller');
+const { viewLoanProfile } = require('../controllers/Loan/loan.controller');
+const { viewPortfolio, addAssets } = require('../controllers/Portfolio/portfolio.controller');
 const router = Router();
 
-router.get('/profile', verifyToken, viewProfile);
-router.post('/profile', verifyToken, updateProfile);
 router.post('/portfolio/add', verifyToken, addAssets);
 router.get('/portfolio', verifyToken, viewPortfolio)
 router.get('/loan', verifyToken, viewLoanProfile);
