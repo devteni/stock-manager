a6642384206cbd6299176b248788dad0b39d91b9
devteni <fatunmbiteniola001@gmail.com>
2021-11-07 22:31:37 +0100

    [feat]: update viewPortfolio controller to send portfolio value with query spec

--- controllers/Portfolio/portfolio.controller.js
+++ controllers/Portfolio/portfolio.controller.js
@@ -1,13 +1,14 @@
 const AppError = require('../../utils/AppError');
-const { decodeUserWithToken } = require('../../services/token.service');
+const { decodeUserWithToken } = require('../../utils/token');
 const Asset = require('../../models/asset.model');
 const {
   addNewAsset,
   updateExistingAsset,
 } = require('../../services/asset.service');
+const { getPortfolioValue } = require('../../services/portfolio.service');
 
 /* Portfolio controllers */
-exports.addAssets = async (req, res, next) => {
+exports.addAssets = async (req, res) => {
   try {
     // get assets from req.body
     const asset_details = req.body;
@@ -44,13 +45,25 @@ exports.addAssets = async (req, res, next) => {
   }
 };
 
-exports.viewPortfolio = async (req, res, next) => {
+exports.viewPortfolio = async (req, res) => {
   try {
+    const { check_value } = req.query;
+
     // get the userID
     let token =
       req.body.token || req.query.token || req.headers['x-access-token'];
     const userID = decodeUserWithToken(token);
     const { id } = userID;
+
+    // condition to get just the portfolio value
+    if(check_value === 'true'){
+      const portfolioVal = await getPortfolioValue(id);
+      if(portfolioVal != null){
+        return res.status(200).json({ status: 'success', message:'Your portfolio value:', data: portfolioVal });
+      } else{
+        throw new AppError(`Error while getting portfolio value`, 500);
+      }
+    }
 
     // find assets with userId = id
     const portfolio = await Asset.find({ userId: id });
@@ -60,7 +73,8 @@ exports.viewPortfolio = async (req, res, next) => {
         message: 'User does not have any asset in portfolio',
       });
     }
-    return res.status(200).json({ status: 'success', data: portfolio });
+
+    return res.status(200).json({ status: 'success', message: 'Your portfolio:', data: portfolio });
   } catch (error) {
     throw new AppError('Error while pulling portfolio records', 500);
   }
