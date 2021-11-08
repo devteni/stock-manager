c0ff85c3b761f8d4f06f46913524f4b9bbd88e3c
devteni <fatunmbiteniola001@gmail.com>
2021-11-08 16:53:38 +0100

    [refactor]: improved error handling messages

--- services/portfolio.service.js
+++ services/portfolio.service.js
@@ -1,17 +1,17 @@
 const Asset = require('../models/asset.model');
 
-const getPortfolioValue = async(id) => {
-    try{
-        const portfolio = await Asset.find({ userId: id });
-        let portfolioVal = 0;
-        let asset;
-        for(asset in portfolio) {
-            portfolioVal += portfolio[`${asset}`].equityValue;  
-        };
-        return portfolioVal;
-    } catch(error){
-        throw new AppError(`Error while getting portfolio value: ${error}`, 500);
+const getPortfolioValue = async (id) => {
+  try {
+    const portfolio = await Asset.find({ userId: id });
+    let portfolioVal = 0;
+    let asset;
+    for (asset in portfolio) {
+      portfolioVal += portfolio[`${asset}`].equityValue;
     }
-}
+    return portfolioVal;
+  } catch (error) {
+    throw new AppError(`Error while getting portfolio value: ${error}`, 500);
+  }
+};
 
 module.exports = { getPortfolioValue };
